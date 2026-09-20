271471aabb238c2a920d294436fbea86e4429ba0
RIG360 <134787168+RIG360@users.noreply.github.com>
2026-09-20 17:55:19 +0200

    commit

---
+++ data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-breit27-hof1-zugang_260912",
+      "name": "Breit27-Hof1-Zugang_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7624104479111047,
+          "pitch": 0.0837532256404856,
+          "rotation": 0,
+          "target": "1-breit27-hof1-unten_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-breit27-hof1-unten_260912",
+      "name": "Breit27-Hof1-unten_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07717055312961563,
+          "pitch": -0.9493179704771357,
+          "rotation": 0,
+          "target": "2-breit27-hof1-oben_260912"
+        },
+        {
+          "yaw": -2.1190907643530377,
+          "pitch": -0.18521176820447138,
+          "rotation": 0,
+          "target": "0-breit27-hof1-zugang_260912"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-breit27-hof1-oben_260912",
+      "name": "Breit27-Hof1-oben_260912",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05505429638028758,
+          "pitch": 0.8375419363898953,
+          "rotation": 3.141592653589793,
+          "target": "1-breit27-hof1-unten_260912"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Breit27-Hof1_260912",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
